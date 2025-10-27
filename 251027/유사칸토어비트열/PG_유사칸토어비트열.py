3aae80e416bcba20b1d3f5a603a728c49645bf23
이규연(Lee Kyuyeon) <lmy6268@gmail.com>
2025-10-27 18:48:22 +0900

    first commit

---
+++ 251027/유사칸토어비트열/PG_유사칸토어비트열.py
@@ -0,0 +1,20 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/148652
+
+def solution(n, l, r):
+    answer = 0
+
+    for i in range(l-1,r):
+        while i>0:
+            # i % 5 == 2 인 위치에 있는 경우,
+            # 0이 해당 위치에 존재함. 
+            # (계속해서 5개의 섹션으로 나누며, 가운데 즉, 2번 인덱스 위치에 있는지 확인 ) 
+            if i % 5 != 2:
+                i//=5 
+            else: 
+                break
+        
+        #1인 경우
+        if i == 0:
+            answer +=1
+
+    return answer
